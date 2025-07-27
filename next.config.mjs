de6b434fdfb614d48509e77aa602bff529d720e7
Robert Sowiński <saironeandonly@gmail.com>
2025-07-27 17:24:50 +0200

    making sure there is Rsuite used in project, final tests/deletes/fixes, disabling devindicators in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true, // Optional
+  devIndicators: false, // Disable the Next.js development overlay
+};
 
 export default nextConfig;
